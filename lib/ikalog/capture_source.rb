84b8d08b41ac298722452e1052836ba6897106a7
r7kamura <r7kamura@gmail.com>
2016-05-24 05:03:04 +0900

    Add code to capture frame from capture source via OpenCV

--- lib/ikalog/capture_source.rb
+++ lib/ikalog/capture_source.rb
@@ -1,11 +1,25 @@
 require "ikalog/frame"
+require "opencv"
 
 module Ikalog
   class CaptureSource
-    # @todo
+    # @param device_id [Fixnum, String, Symbol, nil]
+    # @raise [OpenCV::CvError, StandardError]
+    def initialize(device_id = nil)
+      @device_id = device_id
+      open_cv_capture
+    end
+
     # @return [Ikalog::Frame]
     def read_frame
-      Ikalog::Frame.new
+      Frame.new(@cv_capture.query)
+    end
+
+    private
+
+    # @raise [OpenCV::CvError, StandardError]
+    def open_cv_capture
+      @cv_capture = ::OpenCV::CvCapture.open(@device_id)
     end
   end
 end
--- lib/ikalog/frame.rb
+++ lib/ikalog/frame.rb
@@ -1,4 +1,13 @@
 module Ikalog
   class Frame
+    # @param ipl_image [OpenCV::IplImage]
+    def initialize(ipl_image)
+      @ipl_image = ipl_image
+    end
+
+    # @note Override to ease debugging
+    def to_s
+      "#<#{self.class}::#{object_id} height=#{@ipl_image.height} width=#{@ipl_image.width}>"
+    end
   end
 end
