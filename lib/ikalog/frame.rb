3b8872495e57c2f69bf5f337407355d623c6b220
r7kamura <r7kamura@gmail.com>
2016-05-26 17:12:43 +0900

    Remove unused method

--- lib/ikalog/frame.rb
+++ lib/ikalog/frame.rb
@@ -6,10 +6,5 @@ module Ikalog
     def initialize(ipl_image)
       @ipl_image = ipl_image
     end
-
-    # @return [OpenCV::IplImage]
-    def hsv_image
-      @hsv_image ||= @ipl_image.BGR2HSV
-    end
   end
 end
