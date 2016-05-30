90ac458381647fab80e9e7f29530047170a13ead
r7kamura <r7kamura@gmail.com>
2016-05-31 08:01:31 +0900

    Memoize template image on each Detector instance

--- lib/squid_analyzer/scene_detector.rb
+++ lib/squid_analyzer/scene_detector.rb
@@ -1,15 +1,27 @@
 module SquidAnalyzer
   class SceneDetector
-    # @param options [Hash{Symbol => Object}]
-    def initialize(options)
-      @options = options
+    def initialize(scene_class:, score_threshold:, template_image_path:)
+      @scene_class = scene_class
+      @score_threshold = score_threshold
+      @template_image_path = template_image_path
     end
 
     # @param frame [SquidAnalyzer::Frame]
     # @return [SquidAnalyzer::Scenes::Base, nil]
     def call(frame)
-      keyword_arguments = @options.merge(frame: frame)
-      Detection.new(**keyword_arguments).call
+      Detection.new(
+        frame: frame,
+        scene_class: @scene_class,
+        score_threshold: @score_threshold,
+        template_image: template_image,
+      ).call
+    end
+
+    private
+
+    # @return [OpenCV::IplImage]
+    def template_image
+      @template_image ||= ::OpenCV::IplImage.load(@template_image_path)
     end
 
     class Detection
@@ -17,12 +29,12 @@ module SquidAnalyzer
         frame:,
         scene_class:,
         score_threshold:,
-        template_image_path:
+        template_image:
       )
         @frame = frame
         @scene_class = scene_class
         @score_threshold = score_threshold
-        @template_image_path = template_image_path
+        @template_image = template_image
       end
 
       # @return [SquidAnalyzer::Scenes::Base, nil]
@@ -41,12 +53,7 @@ module SquidAnalyzer
 
       # @return [Float]
       def score
-        @frame.ipl_image.match_template(template_image, ::OpenCV::CV_TM_CCORR_NORMED).min_max_loc[1]
-      end
-
-      # @return [OpenCV::IplImage]
-      def template_image
-        ::OpenCV::IplImage.load(@template_image_path)
+        @frame.ipl_image.match_template(@template_image, ::OpenCV::CV_TM_CCORR_NORMED).min_max_loc[1]
       end
     end
   end
