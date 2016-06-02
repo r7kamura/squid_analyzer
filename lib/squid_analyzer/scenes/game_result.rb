2960026b40f32c998429004a14189b389be957ee
r7kamura <r7kamura@gmail.com>
2016-06-03 03:18:01 +0900

    Recognize whether a player record is mine or not

--- lib/squid_analyzer/scenes/game_result.rb
+++ lib/squid_analyzer/scenes/game_result.rb
@@ -2,18 +2,19 @@ module SquidAnalyzer
   module Scenes
     class GameResult < Base
       DEATHS_COUNT_HEIGHT = 21
-      DEATHS_COUNT_LEFT_IN_PLAYER_ENTRY = 575
+      DEATHS_COUNT_LEFT_IN_PLAYER_RECORD = 575
       DEATHS_COUNT_WIDTH = 31
-      ENTRIES_COUNT_PER_TEAM = 4
       KILLS_COUNT_HEIGHT = DEATHS_COUNT_HEIGHT
-      KILLS_COUNT_LEFT_IN_PLAYER_ENTRY = DEATHS_COUNT_LEFT_IN_PLAYER_ENTRY
+      KILLS_COUNT_LEFT_IN_PLAYER_RECORD = DEATHS_COUNT_LEFT_IN_PLAYER_RECORD
       KILLS_COUNT_WIDTH = DEATHS_COUNT_WIDTH
-      LOSE_PLAYER_ENTRY_TOP = 431
-      PLAYER_ENTRY_HEIGHT = 65
-      PLAYER_ENTRY_INNER_HEIGHT = 45
-      PLAYER_ENTRY_LEFT = 610
-      PLAYER_ENTRY_WIDTH = 610
-      WIN_PLAYER_ENTRY_TOP = 101
+      LEVEL_WIDTH = 43
+      LOSE_PLAYER_RECORD_TOP = 431
+      PLAYER_RECORD_HEIGHT = 65
+      PLAYER_RECORD_INNER_HEIGHT = 45
+      PLAYER_RECORD_LEFT = 610
+      PLAYER_RECORD_WIDTH = 610
+      RECORDS_COUNT_PER_TEAM = 4
+      WIN_PLAYER_RECORD_TOP = 101
 
       # @return [Hash]
       def as_json
@@ -29,17 +30,23 @@ module SquidAnalyzer
       # @todo
       # @param position [Hash{Symbol => Integer}]
       # @return [Hash{Symbol => Hash}]
-      def analyze_player_entry(position)
+      def analyze_player_record(position)
         {
           deaths_count: recognize_digits(
             height: DEATHS_COUNT_HEIGHT,
-            left: position[:left] + DEATHS_COUNT_LEFT_IN_PLAYER_ENTRY,
+            left: position[:left] + DEATHS_COUNT_LEFT_IN_PLAYER_RECORD,
             top: position[:top] + DEATHS_COUNT_HEIGHT,
             width: DEATHS_COUNT_WIDTH,
           ),
+          is_my_record: recognize_is_my_record(
+            height: PLAYER_RECORD_HEIGHT,
+            left: position[:left],
+            top: position[:top],
+            width: LEVEL_WIDTH,
+          ),
           kills_count: recognize_digits(
             height: KILLS_COUNT_HEIGHT,
-            left: position[:left] + KILLS_COUNT_LEFT_IN_PLAYER_ENTRY,
+            left: position[:left] + KILLS_COUNT_LEFT_IN_PLAYER_RECORD,
             top: position[:top],
             width: KILLS_COUNT_WIDTH,
           ),
@@ -47,19 +54,19 @@ module SquidAnalyzer
       end
 
       # @return [Array<Hash>]
-      def lose_player_entry_positions
-        ENTRIES_COUNT_PER_TEAM.times.map do |i|
+      def lose_player_record_positions
+        RECORDS_COUNT_PER_TEAM.times.map do |i|
           {
-            left: PLAYER_ENTRY_LEFT,
-            top: LOSE_PLAYER_ENTRY_TOP + i * PLAYER_ENTRY_HEIGHT,
+            left: PLAYER_RECORD_LEFT,
+            top: LOSE_PLAYER_RECORD_TOP + i * PLAYER_RECORD_HEIGHT,
           }
         end
       end
 
       # @return [Array<Hash>]
       def lose_players
-        lose_player_entry_positions.map do |lose_player_entry_position|
-          analyze_player_entry(lose_player_entry_position)
+        lose_player_record_positions.map do |lose_player_record_position|
+          analyze_player_record(lose_player_record_position)
         end
       end
 
@@ -74,20 +81,31 @@ module SquidAnalyzer
         DigitsRecognition.new(image).call.to_i
       end
 
+      # @param height [Integer]
+      # @param left [Integer]
+      # @param top [Integer]
+      # @param width [Integer]
+      # @return [false, true]
+      def recognize_is_my_record(height:, left:, top:, width:)
+        image = @image.clone
+        image.roi = ::OpenCV::CvRect.new(left, top, width, height)
+        image.BGR2GRAY.threshold(230, 255, ::OpenCV::CV_THRESH_BINARY).count_non_zero.to_f / (image.roi.width * image.roi.height) > 0.1
+      end
+
       # @return [Array<Hash>]
-      def win_player_entry_positions
-        ENTRIES_COUNT_PER_TEAM.times.map do |i|
+      def win_player_record_positions
+        RECORDS_COUNT_PER_TEAM.times.map do |i|
           {
-            left: PLAYER_ENTRY_LEFT,
-            top: WIN_PLAYER_ENTRY_TOP + i * PLAYER_ENTRY_HEIGHT,
+            left: PLAYER_RECORD_LEFT,
+            top: WIN_PLAYER_RECORD_TOP + i * PLAYER_RECORD_HEIGHT,
           }
         end
       end
 
       # @return [Array<Hash>]
       def win_players
-        win_player_entry_positions.map do |win_player_entry_position|
-          analyze_player_entry(win_player_entry_position)
+        win_player_record_positions.map do |win_player_record_position|
+          analyze_player_record(win_player_record_position)
         end
       end
     end
